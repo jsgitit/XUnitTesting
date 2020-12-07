1805c68b0ad2459723f48ff596b76ddfc4807e14
Jon <jsmartware@aol.com>
2020-12-07 11:27:32 -0800

    Added health unit Tests (the long way)

--- GameEngine.Tests/GameStateCollection.cs
+++ GameEngine.Tests/GameStateCollection.cs
@@ -2,6 +2,9 @@
 
 namespace GameEngine.Tests
 {
+    // Create a "fixture" that allows other test to share the GameState collection of data
+    // Use [Collection("GameState collection"] in tests that need to share the data
+    
     [CollectionDefinition("GameState collection")]
     public class GameStateCollection : ICollectionFixture<GameStateFixture> {}
 }
